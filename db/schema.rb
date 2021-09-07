64912da8f7a56e51814bbe802bd7703daaaee43b
StevenDessasore <stevendessasoreba@gmail.com>
2021-09-07 10:12:13 -0400

    Removed Seed Data have to do over

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,18 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_906_070_919) do
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'username'
-    t.string 'email'
-    t.string 'password_digest'
+ActiveRecord::Schema.define(version: 2021_09_06_070919) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
   end
 
-  create_table 'vlogs', force: :cascade do |t|
-    t.string 'name'
-    t.string 'genre'
-    t.text 'description'
-    t.integer 'user_id'
+  create_table "vlogs", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.text "description"
+    t.integer "user_id"
   end
+
 end
