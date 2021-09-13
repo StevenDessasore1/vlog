5f47717b8471a9460aef5859c06ff59d44f9fe71
StevenDessasore <stevendessasoreba@gmail.com>
2021-09-12 20:56:01 -0400

    removing unneccessary files, updating seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
-Vlog.create(name: 'TheStarReport', genre: 'Media News',
+Vlog.find_or_create_by(name: 'TheStarReport', genre: 'Media News',
             description: "Troi Torain aka STAR is a multimedia personality whose career has spanned the arenas of radio, television and the world-wide web. Considered a radio pioneer by many, Torain's personal brand STAR & BUC WILD has set precedents in the urban landscape. STAR made the national stage on MTV in 1999 (Beat Suite) but it was his radio shows on New York's Hot 97 and Power 105 that launched him into super stardom. Torain's resume also includes WEA (Warner Elektra Atlantic), Virgin Records, The Source magazine, Power 104.1 (Hartford), Power 99 (Philly), Hip-Hop Weekly magazine, Vladtv.com, 100.3 The Beat (Philly) shot97.com, VH1 (The Gossip Game) Everyday Struggle (Complex) and The Star Report. In 2011 STAR was also inducted into News Ones Top 20 greatest radio personalities of all time")
 
-Vlog.create(name: 'Casey Neistat', genre: 'Film',
+Vlog.find_or_create_by(name: 'Casey Neistat', genre: 'Film',
             description: 'Casey is undoubtedly one of the best vloggers in the world right now - and has been for years. His vlogs are watched by his devoted fan base of 11 million+ subscribers and have a very unique style. Casey inspires with his hard working attitude and gives wannabe film makers a thrill with his tasteful shots.')
 
-Vlog.create(name: 'Vagabrothers', genre: 'Travel',
+Vlog.find_or_create_by(name: 'Vagabrothers', genre: 'Travel',
             description: "I recently discovered the Vagabrothers, but they are slowly becoming one of my faves to watch. The two brothers started their travels by teaching English in Spain - they make everything accessible and enjoyable for us viewers, and when they're not vlogging, they’re always handing out nifty tips and tricks to help you on your travels. You need to watch the Vagabrothers as they will give you the perfect destination for your next trip - check out the video below to see their ultimate Route 66 trip in the USA.!")
 
-Vlog.create(name: 'VanossGaming', genre: 'Gaming',
+Vlog.find_or_create_by(name: 'VanossGaming', genre: 'Gaming',
             description: 'Another young boy, Evan Fong (born in 1992) is a famous YouTuber. He is of Asian descent but he was raised in Canada. He is a video game commentator. He is known online as VanossGaming or simply Vanoss. Fong created his channel in 2011and by 2015 it had surpassed 10 million subscribers. His channel now has more than 25 million subscribers. Fong says that viewers like the authentic type of his content when asked about the success of this channel.')
 
-Vlog.create(name: 'KSI', genre: 'Entertainment',
+Vlog.find_or_create_by(name: 'KSI', genre: 'Entertainment',
             description: 'Most recently in the headlines for his amateur boxing match with fellow YouTuber, Logan Paul, Olajide William “JJ” Olatunji — KSI to his fans — is a British gamer, rapper, and actor. Creating commentary, sketch, music, and other videos, KSI’s videos have exceeded 4 billion views.')
 
-Vlog.create(name: 'Jasmine Brown', genre: 'Style',
+Vlog.find_or_create_by(name: 'Jasmine Brown', genre: 'Style',
             description: 'Also known as JasMeannnn, model and beauty YouTuber, Jasmine Brown, offers hair and makeup tutorials to her followers. Last year, Brown released a clothing line with Love Culture and an eyelash line with Eylure London.')
+
+
+User.create(name)
